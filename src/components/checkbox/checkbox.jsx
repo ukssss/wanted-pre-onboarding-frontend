bcf1295d6667ea1eab8bde4846fb9fa4dab66de6
ukssss <1872003115su@gmail.com>
2023-08-03 16:13:22 +0900

    FEAT | Checkbox Components

---
+++ src/components/checkbox/checkbox.jsx
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+const Checkbox = () => {
+    return <StyledCheckbox type="checkbox" />;
+};
+
+const StyledCheckbox = styled.input``;
+
+export default Checkbox;
