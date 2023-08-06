e572ea0030648474e01674141953a92f308fdf18
ukssss <1872003115su@gmail.com>
2023-08-06 20:29:10 +0900

    Feat | Implement the todolist add-on
    - Create an input for entering a new todo, create an additional button, and create a todoCreate component as a component that can be used at once

--- src/pages/todo/todo.jsx
+++ src/pages/todo/todo.jsx
@@ -1,28 +1,65 @@
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import TodoTemplate from '../../components/todo/todoTemplate/todoTemplate';
 import TodoList from '../../components/todo/todoList/todoList';
 import TodoItem from '../../components/todo/todoItem/todoItem';
+import TodoCreate from '../../components/todo/todoCreate/todoCreate';
+
+import { DEV_ADDRESS } from '../../api/api';
+import axios from 'axios';
 
 const Todo = () => {
-    // navigate
     const navigate = useNavigate();
 
-    // token
-    const isLoggedIn = localStorage.getItem('token');
+    const access_token = localStorage.getItem('token');
     const onCheckLoggedIn = () => {
-        !isLoggedIn && navigate('/signin');
+        !access_token && navigate('/signin');
     };
 
-    useEffect(onCheckLoggedIn, [isLoggedIn, navigate]);
+    useEffect(onCheckLoggedIn);
+
+    const [todos, setTodos] = useState([]);
+
+    const url = DEV_ADDRESS;
+    const api = axios.create({
+        baseURL: url,
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+            'Content-Type': `application/json`,
+        },
+    });
+
+    const getTodos = async (todo) => {
+        try {
+            const res = await api.get('/todos');
+            setTodos(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        access_token && getTodos();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [access_token]);
+
+    const createTodo = async (todo) => {
+        try {
+            const res = await api.post('/todos', todo);
+            getTodos(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
         <TodoTemplate>
+            <TodoCreate createTodo={createTodo} />
             <TodoList>
-                <TodoItem>테스트1</TodoItem>
-                <TodoItem>테스트2</TodoItem>
-                <TodoItem>테스트3</TodoItem>
+                {todos.map((el) => {
+                    return <TodoItem key={el.id}>{el.todo}</TodoItem>;
+                })}
             </TodoList>
         </TodoTemplate>
     );
