a6b44647c3af92a12e37075c539eb5cb71b7fdbe
ukssss <1872003115su@gmail.com>
2023-08-06 23:05:33 +0900

    Feat | Create todo edit button, delete button

--- src/components/todo/todoItem/todoItem.jsx
+++ src/components/todo/todoItem/todoItem.jsx
@@ -1,21 +1,28 @@
 import { styled, css } from 'styled-components';
-import Checkbox from '../../checkbox/checkbox';
 
-const TodoItem = ({ id, todo, checked, text, onToggle }) => {
+import Checkbox from '../../checkbox/checkbox';
+import Button from '../../button/button';
+
+const TodoItem = ({ id, todo, checked, text, onToggle, onDelete }) => {
     return (
         <StyledItem key={id}>
-            <Checkbox
-                defaultChecked={checked}
-                onChange={() => {
-                    onToggle(todo);
-                }}
-            />
-            <StyledSpan checked={checked}>{text}</StyledSpan>
+            <StyledLabel>
+                <Checkbox
+                    defaultChecked={checked}
+                    onChange={() => {
+                        onToggle(todo);
+                    }}
+                />
+                <StyledSpan checked={checked}>{text}</StyledSpan>
+            </StyledLabel>
+            <Button data-testid="modify-button">수정</Button>
+            <Button data-testid="delete-button">삭제</Button>
         </StyledItem>
     );
 };
 
 const StyledItem = styled.li``;
+const StyledLabel = styled.label``;
 const StyledSpan = styled.span`
     ${(props) =>
         props.checked &&
