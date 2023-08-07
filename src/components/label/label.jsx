388672c6cefb3b049586035708859d61b9772582
ukssss <1872003115su@gmail.com>
2023-08-07 17:19:45 +0900

    Feat | Label Components

---
+++ src/components/label/label.jsx
@@ -0,0 +1,24 @@
+import { styled } from 'styled-components';
+
+const Label = ({ id, children, ...restProps }) => {
+    return (
+        <StyledLabel htmlFor={id} {...restProps}>
+            {children}
+        </StyledLabel>
+    );
+};
+
+const StyledLabel = styled.label`
+    /* sr-only */
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    clip-path: polygon(0 0, 0 0, 0 0);
+    border: 0;
+`;
+
+export default Label;
