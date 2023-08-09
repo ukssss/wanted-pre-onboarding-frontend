b070d91300340d226ff947079e1ba34a587eae37
ukssss <1872003115su@gmail.com>
2023-08-09 16:16:04 +0900

    Refact | Refact to todoList Page

--- src/components/todo/todoTemplate/todoTemplate.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from 'styled-components';
-
-const TodoTemplate = ({ children }) => {
-    return <StyledDiv>{children}</StyledDiv>;
-};
-
-const StyledDiv = styled.div``;
-
-export default TodoTemplate;
